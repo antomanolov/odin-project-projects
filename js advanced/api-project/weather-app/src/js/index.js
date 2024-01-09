c67cc37f2386272cf288d71bfbe3ee4005579730
Anton Manolov <mramanolov@gmail.com>
2024-01-09 07:25:54 +0200

    added background and started adding the html

--- js advanced/api-project/weather-app/src/js/index.js
+++ js advanced/api-project/weather-app/src/js/index.js
@@ -1,4 +1,18 @@
-import '../css/weather-app.css'
+import "../css/weather-app.css";
 
 const h1 = document.querySelector('h1')
-console.log(h1)
+
+async function getWeather(city) {
+    try {
+        const url = `http://api.weatherapi.com/v1/current.json?key=9121f056114546a5bb6120702240401&q=${city}&aqi=no`;
+        const response = await fetch(url);
+        const info = await response.json();
+        
+        
+
+    } catch (error){
+        alert('Please enter valid name')
+    }
+}
+
+getWeather('london')
